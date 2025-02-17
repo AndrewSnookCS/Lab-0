8e8fd0f477cc707c44bc20879cdc8261c1d72883
AndrewSnookCS <112417912+AndrewSnookCS@users.noreply.github.com>
2025-02-16 18:48:57 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    
+    cout << "\nMy name is Andrew Snook" << endl;
+
+    cout << "\nEnter the first number: ";
+    cin >> num1;
+
+    cout << "\nEnter the second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "\nThe sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
+/*
+My name is Andrew Snook
+
+Enter the first number: 5
+
+Enter the second number: 5
+
+The sum of 5 and 5 is 10
+ */
